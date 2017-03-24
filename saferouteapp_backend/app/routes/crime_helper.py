76cd13af38e86d3a517329456a534afc6fc3017b
amrit <amritsahoo@gmail.com>
2017-03-24 16:17:35 -0700

    adding crime api for SF along with raw score calculation

--- saferouteapp_backend/app/routes/crime_helper.py
+++ saferouteapp_backend/app/routes/crime_helper.py
@@ -1,18 +1,59 @@
+key = 'AIzaSyDmNBpYDBoxkwYTW5Aw9H3YrEXaSi-tnAo'
+gmaps = googlemaps.Client(key=key)
+
 def query(slat, slong, dlat, dlong):
-	baseurl = 'https://maps.googleapis.com/maps/api/directions/json?'
-	return "{}origin={},{}&destination={},{}&mode=walking&alternatives=true&key={}".format(baseurl, slat, slong, dlat, dlong, key)
+    baseurl = 'https://maps.googleapis.com/maps/api/directions/json?'
+    return "{}origin={},{}&destination={},{}&mode=walking&alternatives=true&key={}".format(baseurl, slat, slong, dlat, dlong, key)
+
+def get_lat_long(query = '1600 Amphitheatre Parkway, Mountain View, CA'):
+    geocode_result = gmaps.geocode(query)
+    return "{}, {}".format(geocode_result[0][u'geometry'][u'location'][u'lat'], geocode_result[0][u'geometry'][u'location'][u'lng'])
+    # return [geocode_result[0][u'geometry'][u'location'][u'lat'], geocode_result[0][u'geometry'][u'location'][u'lng']]
+
+def get_safe_routes(frm="33.416565,-111.925015", to="33.418000, -111.931827"):
+    """
+    todo: fix better metric, parallelize calls to query_crime with async
+    """
+    routes = gmaps.directions(frm, to, alternatives=True, mode="driving")
+    route_coords = []
+    scores = []
+    for route in routes:
+        legs = route[u'legs'][0]
+        steps = legs[u'steps']
+        starts = [d[u'start_location'] for d in steps]
+        ends = [d[u'end_location'] for d in steps]
+        coords = list(set(map(lambda x: (x[u'lat'], x[u'lng']), starts + ends)))
+        route_coords.append(coords)
+    for coords in route_coords:
+        events = 0
+        for coord in coords:
+            events += query_crime(coords[0], coords[1])
+        print(events)
+        scores.append(events) # / len(coords))
+    return scores
+
+def query_crime(lat, lng, shape='within_circle', rad=500, s_date='2015-01-01', e_date='2015-10-01', granular='ym'):
+    # api usage reference http://www.racketracer.com/2015/10/19/most-frequented-crimes-in-san-francisco-normalized-by-neighborhood/
+    url = "https://data.sfgov.org/resource/cuks-n6tp.json?$select=COUNT(*),category,date_trunc_" + \
+    granular + "(date) as date_key&$where=" + shape + "(location," + str(lat) + "," + str(lng) + "," + str(rad) + ") AND date>" + \
+    "'" + s_date + "' AND date<'" + e_date + "'&$group=date_key,category&$limit=50000"
+    response = requests.get(url).json()
+    df = pd.DataFrame(response)
+    if len(df) == 0:
+        return 0
+    return df.count()[0]
+
+query_crime(37.757396, -122.492781)
+q = ["37.293089, -122.213628", "37.318691, -122.088144"]
+
+a, b = get_lat_long("Cupertino Library, 10800 Torre Ave, Cupertino, CA 95014"), get_lat_long("Amphitheatre Pkwy, Mountain View, CA 94043")
+get_safe_routes(a, b)
+
 
 # query_string("33.416565", "-111.925015", "33.416207", "-111.922558")
-# key = 'AIzaSyDmNBpYDBoxkwYTW5Aw9H3YrEXaSi-tnAo'
 
-def get_safe_routes(query):
-	# gmaps.directions("33.416565,-111.925015", "33.416207, -111.922558")[0]
-	routes = gmaps.directions("33.416565,-111.925015", "33.418000, -111.931827 ", alternatives=True, mode="walking")
-	# routes = gmaps.directions("33.416565,-111.925015", "33.416207, -111.922558")[0][u'legs'][0][u'steps']
-	legs = [d[u'legs'] for d in gmaps.directions("33.416565,-111.925015", "33.416207, -111.922558")][0]
-	steps = [d[u'steps'] for d in legs[0]]
-	starts = [d[u'start_location'] for d in steps[0]]
-
-
-def query_crime(long = -122.444586, lat = 37.782745, radius = 1000):
-	return "https://data.sfgov.org/resource/cuks-n6tp.json?$limit=500&$where=category in('SEX OFFENSES, FORCIBLE','ASSAULT', 'LOITERING','LARCENY/THEFT','KIDNAPPING','WEAPON LAWS','DISORDERLY CONDUCT','DRUNKENNESS','DRUG/NARCOTIC') AND within_circle(location, {}, {}, 1000)&$order=date DESC".format(long, lat)
+# url = query_crime('within_circle', 500, 37.757396, -122.492781, '2015-01-01', '2015-10-01','ym')
+# response = requests.get(url).json()
+# df = pd.DataFrame(response)
+# events = df['COUNT'].count()
+# score = df['COUNT'].map(int).sum() // df['COUNT'].count()
