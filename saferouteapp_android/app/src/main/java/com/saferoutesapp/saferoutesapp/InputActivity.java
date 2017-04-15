eaecb2901d347a230efe20399b227a6d25724790
amrit <amritsahoo@gmail.com>
2017-04-15 00:33:02 -0700

    adding Mainactivity, map and menu layout

---
+++ saferouteapp_android/app/src/main/java/com/saferoutesapp/saferoutesapp/InputActivity.java
@@ -0,0 +1,31 @@
+package com.saferoutesapp.saferoutesapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+
+/**
+ * Created by avniv on 4/14/2017.
+ */
+
+public class InputActivity extends AppCompatActivity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.input_activity);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        super.onCreateOptionsMenu(menu);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    public void onFindRoutes(View v){
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
+}
