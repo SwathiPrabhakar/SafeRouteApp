58efcdf96ffc382964bd822b19b6af4f40478b09
amrit <amritsahoo@gmail.com>
2017-04-16 14:55:43 -0700

    adding base for push notifications 2

---
+++ saferouteapp_android/app/src/main/java/com/saferoutesapp/saferoutesapp/NotificationsListenerService.java
@@ -0,0 +1,11 @@
+package com.saferoutesapp.saferoutesapp;
+
+import com.google.android.gms.gcm.GcmListenerService;
+
+/**
+ * Created by amrit on 4/16/17.
+ */
+
+public class NotificationsListenerService extends GcmListenerService {
+
+}
