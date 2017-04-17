e70d685f6e4954ac4254b0ad018d0681f3ac9188
ajo thomas <ajo.thomas24@gmail.com>
2017-04-17 10:19:22 -0700

    Added autocomplete to destination field in input activity. Changed the layout xml

--- saferouteapp_android/app/src/main/java/com/saferoutesapp/saferoutesapp/InputActivity.java
+++ saferouteapp_android/app/src/main/java/com/saferoutesapp/saferoutesapp/InputActivity.java
@@ -3,6 +3,7 @@ package com.saferoutesapp.saferoutesapp;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -17,16 +18,35 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.content.BroadcastReceiver;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 import android.util.Log;
 import android.Manifest;
+import android.widget.Toast;
 
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.model.LatLng;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+
 /**
  * Created by avniv on 4/14/2017.
  */
+
 
 public class InputActivity extends AppCompatActivity{
     private BroadcastReceiver broadcastReceiver;
@@ -34,8 +54,8 @@ public class InputActivity extends AppCompatActivity{
     final private String TAG = "InputActivity";
     private LocationListener listener;
     private LocationManager locationManager;
-    private LatLng src;
-    private LatLng dest;
+    private LatLng src=null;
+    private LatLng dest=null;
 
     protected void onCreate(Bundle savedInstanceState) {
 //        Intent i = new Intent(this, RegistrationService.class);
@@ -43,6 +63,20 @@ public class InputActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.input_activity);
         from_input = (TextView) findViewById(R.id.from_input);
+
+        AutoCompleteTextView autocompleteView = (AutoCompleteTextView) findViewById(R.id.to_input);
+            autocompleteView.setAdapter(new PlacesAutoCompleteAdapter(this, R.layout.autocomplete_list_item));
+
+        autocompleteView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Get data associated with the specified position
+                // in the list (AdapterView)
+                String description = (String) parent.getItemAtPosition(position);
+                Log.d(TAG, description);
+            }
+        });
+
         if(!runtime_permissions())
             enable_buttons();
     }
@@ -113,6 +147,7 @@ public class InputActivity extends AppCompatActivity{
 
     public void onFindRoutes(View v) {
         // remove this later
+        if(src==null)
         src = new LatLng(33.4284307, -111.9504421);
         dest = new LatLng(33.4236, -111.9393);
         Log.d(TAG, "----------------src :: "+src);
@@ -125,5 +160,127 @@ public class InputActivity extends AppCompatActivity{
 
         startActivity(intent);
 
+    }
+}
+
+class PlaceAPI {
+
+    private static final String TAG = PlaceAPI.class.getSimpleName();
+
+    private static final String PLACES_API_BASE = "https://maps.googleapis.com/maps/api/place";
+    private static final String TYPE_AUTOCOMPLETE = "/autocomplete";
+    private static final String OUT_JSON = "/json";
+
+    private static final String API_KEY = "AIzaSyDmNBpYDBoxkwYTW5Aw9H3YrEXaSi-tnAo";
+
+    public ArrayList<String> autocomplete(String input) {
+        ArrayList<String> resultList = null;
+
+        HttpURLConnection conn = null;
+        StringBuilder jsonResults = new StringBuilder();
+
+        try {
+            StringBuilder sb = new StringBuilder(PLACES_API_BASE + TYPE_AUTOCOMPLETE + OUT_JSON);
+            sb.append("?key=" + API_KEY);
+            sb.append("&types=geocode");
+            sb.append("&input=" + URLEncoder.encode(input, "utf8"));
+
+            URL url = new URL(sb.toString());
+            conn = (HttpURLConnection) url.openConnection();
+            InputStreamReader in = new InputStreamReader(conn.getInputStream());
+
+            // Load the results into a StringBuilder
+            int read;
+            char[] buff = new char[1024];
+            while ((read = in.read(buff)) != -1) {
+                jsonResults.append(buff, 0, read);
+            }
+        } catch (MalformedURLException e) {
+            Log.e(TAG, "Error processing Places API URL", e);
+            return resultList;
+        } catch (IOException e) {
+            Log.e(TAG, "Error connecting to Places API", e);
+            return resultList;
+        } finally {
+            if (conn != null) {
+                conn.disconnect();
+            }
+        }
+
+        try {
+            // Log.d(TAG, jsonResults.toString());
+
+            // Create a JSON object hierarchy from the results
+            JSONObject jsonObj = new JSONObject(jsonResults.toString());
+            JSONArray predsJsonArray = jsonObj.getJSONArray("predictions");
+
+            // Extract the Place descriptions from the results
+            resultList = new ArrayList<String>(predsJsonArray.length());
+            for (int i = 0; i < predsJsonArray.length(); i++) {
+                resultList.add(predsJsonArray.getJSONObject(i).getString("description"));
+            }
+        } catch (JSONException e) {
+            Log.e(TAG, "Cannot process JSON results", e);
+        }
+
+        return resultList;
+    }
+}
+
+class PlacesAutoCompleteAdapter extends ArrayAdapter<String> implements Filterable {
+
+    ArrayList<String> resultList;
+
+    Context mContext;
+    int mResource;
+
+    PlaceAPI mPlaceAPI = new PlaceAPI();
+
+    public PlacesAutoCompleteAdapter(Context context, int resource) {
+        super(context, resource);
+
+        mContext = context;
+        mResource = resource;
+    }
+
+    @Override
+    public int getCount() {
+        // Last item will be the footer
+        return resultList.size();
+    }
+
+    @Override
+    public String getItem(int position) {
+        return resultList.get(position);
+    }
+
+    @Override
+    public Filter getFilter() {
+        Filter filter = new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                FilterResults filterResults = new FilterResults();
+                if (constraint != null) {
+                    resultList = mPlaceAPI.autocomplete(constraint.toString());
+
+                    filterResults.values = resultList;
+                    filterResults.count = resultList.size();
+                }
+
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                if (results != null && results.count > 0) {
+                    notifyDataSetChanged();
+                }
+                else {
+                    notifyDataSetInvalidated();
+                }
+            }
+        };
+
+        return filter;
     }
 }
---
+++ saferouteapp_android/app/src/main/java/com/saferoutesapp/saferoutesapp/PlaceAPI.java
@@ -0,0 +1,20 @@
+package com.saferoutesapp.saferoutesapp;
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+
+/**
+ * Created by ajothomas on 4/17/17.
+ */
+
