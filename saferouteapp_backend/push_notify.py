f13fd523faae9c2151637a71141f93c4f17774ba
amrit <amritsahoo@gmail.com>
2017-04-18 22:11:50 -0700

    modified script for pushnotif, more generic now

--- saferouteapp_backend/push_notify.py
+++ saferouteapp_backend/push_notify.py
@@ -1,19 +1,29 @@
 from gcm import GCM
-API_KEY = 'AIzaSyAEN09LeNCfS92A3_qgXrxIekiKDJC2ets'
+import fire
 
-gcm = GCM(API_KEY)
-data = {'message': 'value1', 'param2': 'value2'}
 
-# Downstream message using JSON request
-reg_ids = ['cZwjGCf2elE:APA91bFXSN0b6vPH79JermZbCPUZGZvw_0odphwJK9VGdh2TPLsElehR0SHpxMYaKD06kknZ8uS5_oqkW0rP0yC8bdT8zrPDJSMWPxCIOj1Ksos9Q0qKkdb0rjczQRI9K_7UjMrPvGps']
-response = gcm.json_request(registration_ids=reg_ids, data=data)
+# usage python2 pusher.py push --message "msg" --reg_id "reg_id"
+class Pusher(object):
+	def push(self, message="test", reg_id=None):
+		API_KEY = 'AIzaSyAEN09LeNCfS92A3_qgXrxIekiKDJC2ets'
 
-# Downstream message using JSON request with extra arguments
-res = gcm.json_request(
-    registration_ids=reg_ids, data=data,
-    collapse_key='uptoyou', delay_while_idle=True, time_to_live=3600
-)
+		print(message)
+		gcm = GCM(API_KEY)
+		data = {'message': message}
 
-# Topic Messaging
-topic = 'topic name'
-gcm.send_topic_message(topic=topic, data=data)
+		# Downstream message using JSON request
+		reg_id = reg_id or 'cZwjGCf2elE:APA91bFXSN0b6vPH79JermZbCPUZGZvw_0odphwJK9VGdh2TPLsElehR0SHpxMYaKD06kknZ8uS5_oqkW0rP0yC8bdT8zrPDJSMWPxCIOj1Ksos9Q0qKkdb0rjczQRI9K_7UjMrPvGps'
+		response = gcm.json_request(registration_ids=[reg_id], data=data)
+
+		# Downstream message using JSON request with extra arguments
+		res = gcm.json_request(
+		    registration_ids=[reg_id], data=data,
+		    collapse_key='uptoyou', delay_while_idle=True, time_to_live=3600
+		)
+
+		# Topic Messaging
+		# topic = 'topic name'
+		# gcm.send_topic_message(topic=topic, data=data)
+
+if __name__ == '__main__':
+  fire.Fire(Pusher)
